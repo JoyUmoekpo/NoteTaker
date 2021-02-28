103a821b49b32c414ecf65aa18b859504e40818c
Joy <joyumoekpo1@gmail.com>
2021-02-27 23:41:43 -0600

    Added reqired routes

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
-// Sets up the Express App
+require("./routes/api");
+require("./routes/html");
 
+// Sets up the Express App
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.listen(PORT, function() {
+    console.log("App listening on: http://localhost:" + PORT);
+  });
