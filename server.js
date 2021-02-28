4e3602e21a2e688ca8d980a7f8e40c9648241338
Joy <joyumoekpo1@gmail.com>
2021-02-28 00:22:40 -0600

    Added static

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-
-require("./routes/api");
-require("./routes/html");
+const route = require("./routes/");
 
 // Sets up the Express App
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(route);
 
 app.listen(PORT, function() {
     console.log("App listening on: http://localhost:" + PORT);
